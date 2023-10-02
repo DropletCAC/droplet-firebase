76b45344923e34e5fb81f68b6a31e05385a2db35
TanujKS <tanujsiripurapu@gmail.com>
2023-10-02 16:19:10 -0700

    Added GPT Tips

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,13 @@
 const { Expo } = require('expo-server-sdk')
 const functions = require("firebase-functions");
+const { defineSecret } = require('firebase-functions/params');
 const admin = require('firebase-admin');
 const fetch = require('node-fetch');
+
+const { OpenAI } = require('openai');
+const OPENAI_API_KEY = defineSecret('OPENAI_API_KEY');
+
+
 admin.initializeApp();
 
 const flask_url = "https://bengal-sought-bedbug.ngrok-free.app/"
@@ -31,8 +37,7 @@ async function sendToExpo(messages) {
   }) ();
 
   return true
-}
-
+};
 
 async function sendNotifications(userId, leak_data) {
   const snap = await admin
@@ -57,6 +62,74 @@ async function sendNotifications(userId, leak_data) {
   return await sendToExpo(messages)
 };
 
+
+async function generateTip(userId) {
+    const apiKey = OPENAI_API_KEY.value()
+    const openai = new OpenAI({
+      apiKey: apiKey,
+    });
+    
+    const data = {}
+    const today = new Date()
+
+    const meters = []
+    const querySnapshot = await admin
+      .firestore()
+      .collection("users")
+      .doc(userId)
+      .collection("meters")
+      .get();
+    
+    querySnapshot.forEach((doc) => {
+      meters.push(doc.id)
+    });
+
+
+    for await (const meter of meters) {
+        const snap = await admin 
+          .firestore()
+          .collection("users")
+          .doc(userId)
+          .collection("meters")
+          .doc(meter)
+          .collection("usage")
+          .doc("2023")
+          .get()
+        
+        
+        data[meter] = snap.data()[(today.getMonth() + 1).toString()]
+    
+    }
+
+    console.log(meters, data)
+    const chatCompletion = await openai.chat.completions.create({
+      messages: [{ role: 'user', content: `${data}\n\n\n
+      Above is data for household water usage for this month in JSON format. The data is broken down into each respective section of the house, followed by the day represented as a number from 1-31 which links to a 24-item long integer indicating how much water was used each hour. 
+      \nYou are an inspector for the local company and you have been asked to come up with a, concise, one-sentence, but specific helpful tip to help this household conserve its water. Your tip must be backed up by actual data that was provided. 
+      \nExample 1: "50% of your water usage this month was from your lawn! Try turning it down this month!" 
+      \nExample 2: "Your water usage peaks at 5 PM every day, try reducing your usage at this time` }],
+      model: 'gpt-3.5-turbo',
+    });
+  
+    console.log(chatCompletion.choices);
+
+    await admin 
+      .firestore()
+      .collection("users")
+      .doc(userId)
+      .collection("tips")
+      .add({
+        content: chatCompletion.choices[0].message.content,
+        date: today,
+      })
+}
+
+exports.tips = functions.https.onRequest(async(request, response) => {
+  await generateTip("BwyZV2GQN0O1DVDsGl4BAj9W5q92")
+  response.send("ok")
+});
+
+
 exports.updateUsage = functions.firestore.document("users/{userId}/meters/{meter}").onUpdate(async (change, context) => {
     console.log("UPDATED")
     console.log("Meter Changed:", change.after.data());
@@ -70,17 +143,23 @@ exports.updateUsage = functions.firestore.document("users/{userId}/meters/{meter
     const meter = change.after.id;
     const userId = change.after.ref.parent.parent.id 
 
-    console.log("Attempting to access:", month, day, hour)
     const usage_ref = change.after.ref.collection("usage").doc(today.getFullYear().toString())
     const usage_snap = await usage_ref.get();
     let data = usage_snap.data();
     
-    const previousUsage = data[today.getMonth() + 1][today.getDate()][hour];
+    const previousUsage = data[month][day][hour];
 
     if (!previousUsage) {
       console.log("New Hour... Checking for leaks")
-
-      console.log(meter, userId, month, day, hour)
+      
+      console.log("Checking to see if halfway through month...")
+      if (month > 15) {
+        console.log("Rolling dice...")
+        let dice = Math.floor(Math.random() * 10)
+        if (dice == 9) {
+          await generateTip(userId)
+        }
+      }
 
       const response = await fetch(`${flask_url}leak?user=${userId}&section=${meter}&month=${month}&day=${day}&hour=${hour - 1}`);
 
@@ -101,11 +180,11 @@ exports.updateUsage = functions.firestore.document("users/{userId}/meters/{meter
         await sendNotifications(userId, leak_data.leak)
       }
 
-      data[today.getMonth() + 1][today.getDate()][hour] = 0
+      data[month][day][hour] = 0
     }
 
-    data[today.getMonth() + 1][today.getDate()][hour] = (parseInt(data[today.getMonth() + 1][today.getDate()][hour]) + parseInt(currentUsage));
-    console.log("Hourly Meter Usage Updated", previousUsage, data[today.getMonth() + 1][today.getDate()][hour]);
+    data[month][day][hour] = (parseInt(data[month][day][hour]) + currentUsage);
+    console.log("Hourly Meter Usage Updated", previousUsage, data[month][day][hour]);
 
     await usage_ref.set(data);
     
@@ -114,14 +193,14 @@ exports.updateUsage = functions.firestore.document("users/{userId}/meters/{meter
     const total_usage_snap = await total_usage_ref.get();
     let totalData = total_usage_snap.data();
 
-    const previousTotalUsage = totalData[today.getMonth() + 1][today.getDate()][hour];
+    const previousTotalUsage = totalData[month][day][hour];
     if (!previousTotalUsage) {
-      console.log("shit is nan, setting to 0")
-      totalData[today.getMonth() + 1][today.getDate()][hour] = 0
+      console.log("Total Usage entry for this hour does not exist, setting to 0")
+      totalData[month][day][hour] = 0
     }
 
-    totalData[today.getMonth() + 1][today.getDate()][hour] = (parseInt(totalData[today.getMonth() + 1][today.getDate()][hour]) + parseInt(currentUsage));
-    console.log("Total Usage Updated", previousTotalUsage, totalData[today.getMonth() + 1][today.getDate()][hour]);
+    totalData[month][day][hour] = (parseInt(totalData[month][day][hour]) + currentUsage);
+    console.log("Total Usage Updated", previousTotalUsage, totalData[month][day][hour]);
 
     await total_usage_ref.set(totalData)
 
